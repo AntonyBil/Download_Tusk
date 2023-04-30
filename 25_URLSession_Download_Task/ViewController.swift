d18a4cbdf2220719ea17378ee935b42c96345bf4
AntonyBil <beletskiy.ant@gmail.com>
2023-04-30 12:34:20 +0300

    create UI

--- 25_URLSession_Download_Task/ViewController.swift
+++ 25_URLSession_Download_Task/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var downloadImageView: UIImageView!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var progresLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func downloadButtonTapped(_ sender: UIButton) {
+    }
 }
 
